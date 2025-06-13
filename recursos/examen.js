798542480b6caa665d88dc683ac60e27394f3c73
David <davsermel@gmail.com>
2025-06-13 09:54:00 +0200

    colors

--- recursos/examen.js
+++ recursos/examen.js
@@ -1,9 +1,19 @@
-const toastTrigger = document.getElementById('liveToastBtn')
-const toastLiveExample = document.getElementById('liveToast')
+document.addEventListener('DOMContentLoaded', () => {
+  const toastTrigger = document.getElementById('liveToastBtn');
+  const toastLiveExample = document.getElementById('liveToast');
 
-if (toastTrigger) {
-  const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toastLiveExample)
-  toastTrigger.addEventListener('click', () => {
-    toastBootstrap.show()
-  })
-}
+  if (toastTrigger && toastLiveExample) {
+    const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toastLiveExample);
+
+    toastTrigger.addEventListener('click', () => {
+      toastBootstrap.show();
+
+      if (window.innerWidth <= 600) {
+        toastTrigger.style.display = 'none';
+      }
+    });
+    toastLiveExample.addEventListener('hidden.bs.toast', () => {
+      toastTrigger.style.display = 'block';
+    });
+  }
+});
